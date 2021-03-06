bf106178b202f5500137bea80bb1ca80dd2fb7ff
Musa Kuşbey <78588618+Musakusbey@users.noreply.github.com>
2021-03-07 02:05:58 +0300

    Add files via upload
    BastanNesneYonelimliProgramlama10-FonksiyonlarinAldigiParametreler

---
+++ BaştanNesneYonelımlıProgramlama10-FonksiyonlarınAldığıParametreler.cpp
@@ -0,0 +1,96 @@
+//Fonksiyonların veya Metodların Aldıpı Parametreler 3dür
+
+/* 
+0->Değer ile geçirmek.
+
+1->Referans ile geçirmek.
+
+2->İşaretçi ile geçirmek.
+
+*/
+
+/*
+#include<iostream>
+                            //Değer ile geçirmek
+using namespace std;
+
+void degerlegecir(int yenisayi)
+{
+	yenisayi=42;
+}
+
+int main ()
+{
+	int sayi=3;
+	
+	degerlegecir(sayi);
+	
+	std::cout<<"sayi"<<sayi<<std::endl;
+	
+	return 0;
+}
+
+*/
+
+/*
+#include<iostream>
+                            //Referans ile geçirmek
+using namespace std;
+
+void referanslagecir(int&yenisayi)
+{
+	yenisayi=42;
+}
+
+int main ()
+{
+	int sayi=3;
+	
+	referanslagecir(sayi);
+	
+	std::cout<<"sayi"<<sayi<<std::endl;
+	
+	return 0;
+}
+
+*/
+
+/*
+#include<iostream>
+                                 //pointer(işaretçi) ile geçirmek	   
+using namespace std;
+
+void isaretcigecir(int*yenisayi)
+{
+	*yenisayi=42;
+}
+
+int main ()
+{
+	int sayi=3;
+	
+	isaretcigecir(&sayi);
+	
+	std::cout<<"sayi:"<<sayi<<std::endl;
+	
+	return 0;
+}
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
